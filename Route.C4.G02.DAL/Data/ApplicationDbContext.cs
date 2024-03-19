2af8be52e0981c674c8ae811065c9723ebc8fb37
Ofaa85 <youssefroaf94@gmail.com>
2024-03-19 23:59:02 +0200

    DAL For Department Module

---
+++ Route.C4.G02.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C4.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C4.G02.DAL.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=.;Database = MVCApplicationG02; Trusted_Connection = True; Encrypt=False ");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public DbSet<Department> Departments{ get; set; }
+    }
+}
---
+++ Route.C4.G02.DAL/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C4.G02.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+    }
+}
