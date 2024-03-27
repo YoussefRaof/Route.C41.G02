283fb36d48da91f5eb9e30c71dc1cbc14da68a34
Ofaa85 <youssefroaf94@gmail.com>
2024-03-27 21:52:44 +0200

    EmployeeController-View

--- Route.C41.G02.PL/Startup.cs
+++ Route.C41.G02.PL/Startup.cs
@@ -44,6 +44,7 @@ namespace Route.C41.G02.PL
             });
 
             services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
             
             
         }
